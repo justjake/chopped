ecb74bd7ae01df774490f3d9f261b6a946415726
Jake Teton-Landis <jake.tl@airbnb.com>
2016-04-21 03:39:31 -0700

    factorio runit cookbook. mostly configurable!

---
+++ site-cookbooks/factorio/attributes/default.rb
@@ -0,0 +1,27 @@
+default.factorio.debug = false
+
+# installation settings
+default.factorio.version = '0.12.29'
+default.factorio.download_uri = 'https://www.factorio.com/get-download/%{version}/headless/linux64'
+default.factorio.tmp_location '/tmp/factorio-install-process'
+
+
+default.factorio.install_location = '/opt/factorio/app'
+default.factorio.save_location = '/opt/factorio/storage'
+default.factorio.config_location = '/opt/factorio/config'
+default.factorio.binary = '%{install_location}/bin/x64/factorio'
+
+# network
+default.factorio.port = 34197
+default.factorio.latency = 250
+
+# save settings
+default.factorio.autosave_interval = 10
+default.factorio.autosave_slots = 10
+default.factorio.save_name = 'chef-default-save'
+
+# Add extra arguments when we run the factorio server
+# example: '--disallow-commands --peer-to-peer'
+default.factorio.extra_binargs = ''
+
+# TODO: handle config of factorio via attributes, or via template?
--- site-cookbooks/factorio/recipes/default.rb
+++ site-cookbooks/factorio/recipes/default.rb
@@ -6,3 +6,86 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# create a service user. We do not want to run Factorio as root.
+user 'factorio' do
+  password '*'
+  system true
+  home '/dev/null'
+end
+
+# can we put a big if around all this checking if we need to perform an install?
+directory 'tmp' do
+  path node.factorio.tmp_location
+  recursive true
+end
+
+download_uri = node.factorio.download_uri % {version: node.factorio.version}
+
+remote_file 'download' do
+  source download_uri
+  path File.join(node.factorio.tmp_location, 'factorio.tar.gz')
+end
+
+bash 'install' do
+  user 'root'
+  cwd node.factorio.tmp_location
+  creates 'maybe'
+  code <<-EOH
+    tar xzf factorio.tar.gz
+    mv factorio #{node.factorio.install_location}
+  EOH
+end
+
+### make sure everything has the right permissions
+directory '/opt' do
+  not_if { File.exist?('/opt') }
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+directory node.factorio.install_location do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+end
+
+directory node.factorio.save_location do
+  owner 'factorio'
+  group 'factorio'
+  recursive true
+  mode '0755'
+end
+
+directory node.factorio.config_location do
+  owner 'factorio'
+  group 'factorio'
+  recursive true
+  mode '0755'
+end
+
+### make sure factorio goes looking for the config file in the right spot
+template 'config-path' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  source 'config-path.cfg.erb'
+  path ::File.join(node.factorio.install_location, 'config-path.cfg')
+  # TODO: notifies runit_service[factorio] ?
+end
+
+template 'config' do
+  owner 'factorio'
+  group 'factorio'
+  mode '0755'
+  source 'config.ini.erb'
+  path ::File.join(node.factorio.config_location, 'config.ini')
+  # TODO: notifies runit_service[factorio] ?
+end
+
+### set up runit service
+runit_service "factorio" do
+  default_logger true
+end
