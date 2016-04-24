05bd30fbe809c93b6a51c3f88ee4207f67a51b93
Jake Teton-Landis <jake.tl@airbnb.com>
2016-04-24 13:47:07 -0700

    [nginx] improve nginx comments

--- site-cookbooks/chopped_nginx/libraries/chopped_nginx_providers.rb
+++ site-cookbooks/chopped_nginx/libraries/chopped_nginx_providers.rb
@@ -1,5 +1,32 @@
-module Chopped
+# TODO: rename file to something else
 
+module Chopped
+  # CombinedScope is an alternative to calling dsl_obj.instance_eval directly on
+  # a DSL implementation object. If you wrap your dsl object in a CombinedScope,
+  # you can evaluate blocks in a way that retains their initial scope.
+  #
+  # Example:
+  # # in your library
+  # def dsl(&block)
+  #   dsl_obj = MyModule::DSL.new
+  #   scope = CombinedScope.new(dsl_obj)
+  #   scope.evaluate(block)
+  # end
+  #
+  # # in a user's class
+  # class Foo
+  #   def expensive_calculation
+  #     @result ||= perform_calculation
+  #   end
+  #   def use_dsl
+  #     dsl do
+  #       # you can access these methods on the parent scope's `self`
+  #       # this is usually not possible with instance_eval based DSLs
+  #       use expensive_cacluation
+  #       use_again expensive_calculcation
+  #     end
+  #   end
+  # end
   class CombinedScope
     def initialize(child)
       @child = child
@@ -50,7 +77,6 @@ module Chopped
       define_method(name) do |value = Chef::NOT_PASSED, &block|
         # set when a block is given
         if !block.nil?
-          Chef::Log.info("property #{name} recieved block #{block}")
           self.send(base_impl, block)
         end
 
