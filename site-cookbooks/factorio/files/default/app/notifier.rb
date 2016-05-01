a66538e738936b551175623acf3f6ea228cb2553
Jake Teton-Landis <jake.tl@airbnb.com>
2016-05-01 15:46:20 -0700

    [factorio-api] add system to parse factorio log lines

---
+++ site-cookbooks/factorio/files/default/app/notifier.rb
@@ -0,0 +1,10 @@
+require 'yaml'
+require_relative './lib/factorio'
+
+APP_DIR = File.dirname(__FILE__)
+CONFIG_PATH = ENV['CONFIG'] || File.join(APP_DIR, 'config.yml')
+CONFIG = YAML::load(File.read(CONFIG_PATH))
+factorio = Factorio.new(CONFIG)
+
+LOG_FILE = '/var/log/factorio/current'
+factorio.players.tail_file(log_file)
