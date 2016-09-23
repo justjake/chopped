9c0cbedbc1fb02ff30ff2d044956deba13bfec3c
Jake Teton-Landis <jake.tl@airbnb.com>
2016-09-23 15:37:04 -0700

    restart notifier

--- site-cookbooks/factorio/recipes/api.rb
+++ site-cookbooks/factorio/recipes/api.rb
@@ -12,6 +12,7 @@ remote_directory node.factorio.api.install_location do
   group 'root'
   mode '0755'
   notifies :restart, "runit_service[factorio-api]", :delayed
+  notifies :restart, "runit_service[factorio-notifier]", :delayed
 end
 
 file 'config' do
